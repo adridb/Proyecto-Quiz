98b646338d074cb679c78c4371b5229a016af4da
adridb <adrian.villar.estevez@alumnos.upm.es>
2016-05-19 18:21:29 +0200

    Tema 10

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,35 +1,46 @@
 var models = require('../models');
 
-// GET /question
-exports.question = function(req,res,next){
+
+
+// GET /quizzes
+exports.index = function(req, res, next) {
+	models.Quiz.findAll()
+		.then(function(quizzes) {
+			res.render('quizzes/index.ejs', { quizzes: quizzes});
+		})
+		.catch(function(error) {
+			next(error);
+		});
+};
+
+// GET /quizzes/:id
+exports.show = function(req,res,next){
 	models
 	.Quiz
-	.findOne() // busca la primera pregunta
+	.findById(req.params.quizId) // busca la primera pregunta
 	.then(function(quiz){
 		if (quiz){
 			var answer = req.query.answer || '';
-            res.
-            render('quizzes/question',{question:quiz.question, answer:answer});
+            res.render('quizzes/show',{quiz:quiz, answer:answer});
 		}
 		else{
-			throw new Error('No hay preguntas en la BBDD');
+			throw new Error('No existe este quiz en la BBDD.');
 		}
 	}).catch(function(error){next(error);});	
 };
-// GET /check
+// GET /quizzes/id:/check
 exports.check = function(req,res,next){
 	models
 	.Quiz
-	.findOne() // busca la primera pregunta
+	.findById(req.params.quizId) // busca la primera pregunta
 	.then(function(quiz){
 		if (quiz){
 	var answer = req.query.answer || '';
 	var result = answer== quiz.answer ? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result',{result: result,
-	                             answer: answer});
+	res.render('quizzes/result',{quiz : quiz, result: result, answer: answer});
         }
 	else{
-			throw new Error('No hay preguntas en la BBDD');
+			throw new Error('No existe este quiz en la BBDD.');
 		}
 	}).catch(function(error){next(error);});	
 };
